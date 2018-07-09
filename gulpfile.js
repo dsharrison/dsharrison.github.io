48e899c137c799c54ac76354fbbeac13f2cb1d00
Derrek Harrison <derrek.harrison@samsara.com>
2018-07-08 21:43:08 -0700

    production deploy: 7/8/2018, 9:43:08 PM

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function(done) {
   gulp.src(['./*', '!node_modules'])
       .pipe(git.commit(
           `production deploy: ${new Date(Date.now()).toLocaleString()}`));
 
   return git.push('origin', 'master', function(err) {
     if (err) throw err;
+    done();
   });
 });
